c155f8fc9ba9dd33ad37c440bd411cc7c0f3ad64
20377028 <20377028@buaa.edu.cn>
2023-12-05 16:06:46 +0800

    team dynamic list

--- app/src/main/java/com/example/myapplication/ui/team/TeamFragment.java
+++ app/src/main/java/com/example/myapplication/ui/team/TeamFragment.java
@@ -5,38 +5,55 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.myapplication.Bean.SportsBean;
+import com.example.myapplication.Bean.TeamBean;
+import com.example.myapplication.Bean.UserBean;
+import com.example.myapplication.Dao.TeamDao;
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentTeamBinding;
 import com.example.myapplication.ui.sport.MoreActivity;
 
-public class TeamFragment extends Fragment implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TeamFragment extends Fragment {
 
     private FragmentTeamBinding binding;
+    private ListView listView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentTeamBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        listView=root.findViewById(R.id.team_list_view);
+        TeamDao teamDao = new TeamDao(this.getContext());
+        List<TeamBean> teamlist = null;
+        teamlist = teamDao.queryAll();
+//        UserBean u = new UserBean("user","user");
+//        SportsBean s = new SportsBean(0,"sport","start",u);
+//        TeamBean t = new TeamBean(0,s,u);
+        if(teamlist==null) teamlist=new ArrayList<>();
+        TeamAdapter adapter=new TeamAdapter(this.getContext(),R.layout.team_item,teamlist);
+        listView.setAdapter(adapter);
 
         return root;
     }
-
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
-    }
-
-    @Override
-    public void onClick(View view) {
-
     }
 }
