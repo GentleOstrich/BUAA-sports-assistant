b37af0c7755ea8349d06a30f7a0209651549fb53
王搏 <21373316@buaa.edu.cn>
2023-12-05 15:52:15 +0800

    add logout

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.myapplication.ui.sport.BadmintonActivity;
 import com.example.myapplication.ui.sport.BasketballActivity;
@@ -29,9 +30,17 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.myapplication.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity  {
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    private MenuItem item1;
+
+    private MenuItem item2;
+
+    private MenuItem item3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,19 +67,59 @@ public class MainActivity extends AppCompatActivity  {
 
     public boolean onCreateOptionsMenu(Menu menu) {
         //第二个参数表示此menu的id值，在onOptionsItemSelected方法中通过id值判断是哪个menu被点击了
-        menu.add(Menu.NONE, 1, 1, "登录");
+        item1 = menu.add(Menu.NONE, 1, 1, "登录");
         return true;
     }
+
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        if (menu != null) {
+            String account = getSharedPreferences("user", MODE_PRIVATE).getString("phone", "none");
+            if (!account.equals("none")) {
+                item1.setTitle("退出登录");
+                if(menu.findItem(2)==null) {
+                    item2 = menu.add(Menu.NONE, 2, 2, "发布组局");
+                    item3 = menu.add(Menu.NONE, 3, 3, "发布攻略");
+                }
+            } else {
+                item1.setTitle("登录");
+                MenuItem itemToRemove = menu.findItem(2);
+                if (itemToRemove != null) {
+                    menu.removeItem(2);
+                }
+                itemToRemove = menu.findItem(3);
+                if (itemToRemove != null) {
+                    menu.removeItem(3);
+                }
+
+            }
+        }
+        return super.onMenuOpened(featureId, menu);
+    }
+
 
     //点击实现的操作
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case 1:
-                startActivity(new Intent(this, LoginSQLiteActivity.class));
+                String account = getSharedPreferences("user", MODE_PRIVATE).getString("phone", "none");
+                if (account.equals("none")) {
+                    startActivity(new Intent(this, LoginSQLiteActivity.class));
+                } else {
+                    SharedPreferences sharedPreferences = getSharedPreferences("user", 0);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.remove("phone");
+                    editor.apply();
+                }
                 break;
+            case 2:
 
+                break;
+            case 3:
+                break;
         }
         return true;
     }
+
 
 }
