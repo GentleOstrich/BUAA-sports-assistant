4efbc95a61b4cb8075b7605d27117cdd32e9b9e4
GentleOstrich <273819111@qq.com>
2023-12-06 15:02:03 +0800

    fix: listener

--- app/src/main/java/com/example/myapplication/ui/team/TeamCreate.java
+++ app/src/main/java/com/example/myapplication/ui/team/TeamCreate.java
@@ -1,9 +1,14 @@
 package com.example.myapplication.ui.team;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,7 +20,9 @@ import com.example.myapplication.Dao.UserDao;
 import com.example.myapplication.R;
 
 public class TeamCreate extends AppCompatActivity implements View.OnClickListener {
-    private TextView teamCreateSport,teamCreateTime,teamCreateLocation;
+    private EditText teamCreateSport, teamCreateTime, teamCreateLocation;
+    private Button btn_create;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.team_create);
@@ -26,7 +33,12 @@ public class TeamCreate extends AppCompatActivity implements View.OnClickListene
         teamCreateSport = findViewById(R.id.team_create_sport); // 创建组局的运动类型
         teamCreateTime = findViewById(R.id.team_create_time); // 创建组局的运动时间
         teamCreateLocation = findViewById(R.id.team_create_location); // 创建组局的运动地点
-        findViewById(R.id.btn_team_create).setOnClickListener(this);
+        btn_create = findViewById(R.id.btn_team_create);
+
+        teamCreateSport.addTextChangedListener(new HideTextWatcher());
+        teamCreateTime.addTextChangedListener(new HideTextWatcher());
+        teamCreateLocation.addTextChangedListener(new HideTextWatcher());
+        btn_create.setOnClickListener(this);
 
     }
 
@@ -42,14 +54,39 @@ public class TeamCreate extends AppCompatActivity implements View.OnClickListene
     }
 
     public void onClick(View v) {
-        String sport = teamCreateSport.getText().toString();
-        String time = teamCreateTime.getText().toString();
-        String location = teamCreateLocation.getText().toString();
-        String account = getSharedPreferences("user", MODE_PRIVATE).getString("account", "none");
-        UserDao userDao = new UserDao(this);
-        UserBean curUserBean = userDao.queryByAccount(account).get(0);
-        TeamBean teamBean = new TeamBean(sport,curUserBean,time,location);
-        TeamDao teamDao = new TeamDao(this);
-        teamDao.insert(teamBean);
+        if (v.getId() == R.id.btn_team_create) {
+            String sport = teamCreateSport.getText().toString();
+            String time = teamCreateTime.getText().toString();
+            String location = teamCreateLocation.getText().toString();
+            String account = getSharedPreferences("user", MODE_PRIVATE).getString("account", "none");
+            UserDao userDao = new UserDao(this);
+            UserBean curUserBean = userDao.queryByAccount(account).get(0);
+            TeamBean teamBean = new TeamBean(sport, curUserBean, time, location);
+            TeamDao teamDao = new TeamDao(this);
+            teamDao.insert(teamBean);
+            int b = teamDao.queryAll().size();
+            Toast.makeText(this, "您已创建该组局", Toast.LENGTH_SHORT).show();
+        }
     }
+
+    private class HideTextWatcher implements TextWatcher {
+        public HideTextWatcher() {
+            super();
+        }
+
+        // 在编辑框的输入文本变化前触发
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        // 在编辑框的输入文本变化时触发
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        // 在编辑框的输入文本变化后触发
+        public void afterTextChanged(Editable s) {
+
+        }
+    }
+
+
 }
