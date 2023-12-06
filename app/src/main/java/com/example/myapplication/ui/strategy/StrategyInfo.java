88b1f7178d764a4c63003940e8d99f6524ca94fd
王搏 <21373316@buaa.edu.cn>
2023-12-06 16:34:02 +0800

    add strategy_info image

--- app/src/main/java/com/example/myapplication/ui/strategy/StrategyInfo.java
+++ app/src/main/java/com/example/myapplication/ui/strategy/StrategyInfo.java
@@ -2,12 +2,16 @@ package com.example.myapplication.ui.strategy;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Base64;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -34,6 +38,9 @@ public class StrategyInfo extends AppCompatActivity {
 
     private TextView title;
     private TextView content;
+
+    private ImageView image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,12 +49,18 @@ public class StrategyInfo extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_back);
 
+        image = findViewById(R.id.strategy_info_image);
         title = findViewById(R.id.strategy_info_title);
         content = findViewById(R.id.strategy_info_content);
         int strategyId = getIntent().getIntExtra("data", -1);
         if (strategyId != -1) {
             title.setText(new StrategyDao(this).queryById(strategyId).getTitle());
             content.setText(new StrategyDao(this).queryById(strategyId).getContent());
+            if (new StrategyDao(this).queryById(strategyId).getImage() != null) {
+                byte[] decodedBytes = Base64.decode(new StrategyDao(this).queryById(strategyId).getImage(), Base64.DEFAULT);
+                Bitmap bitmap = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+                image.setImageBitmap(bitmap);
+            }
         }
 
     }
